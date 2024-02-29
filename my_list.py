c29cd54f7c42603a42b6d2c711231a06db0e235b
solikaivan <160485699+solikaivan@users.noreply.github.com>
2024-02-29 13:22:09 +0300

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,14 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.append(50)
+my_list.append(60)
+my_list.append(70)
+
+my_list.pop()
+
+index_of_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_of_30)
